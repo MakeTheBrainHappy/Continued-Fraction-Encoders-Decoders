6f824e1e2b576f66e5d1e552b16d4db804c017fa
MakeTheBrainHappy <makethebrainhappy@gmail.com>
2019-03-12 17:36:32 -0400

    Added a superposition graph

--- Standard_Continued_Fraction_Projects/StandardCFGenerator.py
+++ Standard_Continued_Fraction_Projects/StandardCFGenerator.py
@@ -16,9 +16,7 @@ def main():
         elif len(b) > 0:
             b[-1] += i.strip()
     b = list(map(Decimal, b))
-    
-    print(b)
-    
+        
     bList = [] # Stores the b values for the continued fraction
     for i in range(0,100):
         bList.append([])
@@ -26,16 +24,17 @@ def main():
             bList[i].append(math.floor(b[i]))
             b[i] = 1/(b[i] - Decimal(b[i]).quantize(Decimal('1.'), rounding=ROUND_DOWN))
     
-    #for i in range(0,30):
-        #temp = []
-        #for j in range (0,100):
-            #temp.append(bList[j][i])
-        #plt.plot(temp)
-        #plt.xlabel('the _ non-trivial zero (i.e. 1st, 2nd, 3rd ...)')
-        #plt.ylabel('Value')
-        #plt.title(str(i+1) + '- Riemann Zeta Zero Standard CF Term')
-        #plt.savefig("RiemannZetaZeroesTerm" + str(i+1) + "StandardCFPattern" + ".png")
-        #plt.show()
+    for i in range(0,30):
+        temp = []
+        for j in range (0,100):
+            temp.append(bList[j][i])
+        plt.plot(temp)
+    plt.xlabel('the _ non-trivial zero (i.e. 1st, 2nd, 3rd ...)')
+    plt.ylabel('Value')
+    plt.title("Riemann Zeta Zero Standard CF Term Superposition (Zeroes 1-100)")
+    plt.savefig("RiemannZetaZeroesTerm" + "StandardCFPatternSuperposition" + ".png")
+    
+    plt.show()
 
     #f= open("LiebsSquareIceConstantStandardCF.txt","w+")
     #for i in range(0, 1000):
